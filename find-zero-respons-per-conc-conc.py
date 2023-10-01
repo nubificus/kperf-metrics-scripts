bccc33cd82ee413503f65f09a41b65250d6c2b01
Ioannis Plakas <iplakas@nubificus.co.uk>
2023-10-01 15:24:16 +0300

    Update collecting script

--- find-zero-respons-per-conc-conc.py
+++ find-zero-respons-per-conc-conc.py
@@ -15,4 +15,14 @@ df['count_ksvcs'] = df['File'].str.extract(r'ksvcs_(\d+)')
 # then count the occurrences of [200] responses equal to 0
 zero_200_response_count = df[df['[200] responses'] == 0].groupby(['conc_step', 'cont_runtime', 'count_ksvcs']).size().reset_index(name='count_no_resp')
 
+
+# Specify the output file path in your Python directory
+output_csv_path = "/home/plakic/playground/nubificus/kperf-metrics-scripts/csv-metrics/count-no-resp-per-service.csv"
+
+
+# Save the result to a CSV file in the Python directory
+zero_200_response_count.to_csv(output_csv_path, index=False)
+
+print(f"Output saved to {output_csv_path}")
+
 print(zero_200_response_count)
